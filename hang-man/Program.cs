2b4c290f311b049a050dd14c3a28c22cececc495
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-04-02 23:31:35 +1200

    created arrays for working out word

--- hang-man/Program.cs
+++ hang-man/Program.cs
@@ -7,6 +7,9 @@ namespace hangman
         public static void Main(string[] args)
         {
             string wordToGuess = "frog";
+            var wordAsArray = wordToGuess.Split('');
+            string[] workingArray = new string[0];
+            string displayWord = "";
             int playerTurnCount = 0;
             Console.WriteLine("Let's play Hangman! Guess the word.");
         }
