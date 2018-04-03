0c16ebf64ec2c7eba11c0f712eccfdadb42d031e
Callan Stretton <callanstretton@Callans-MacBook-Pro.local>
2018-04-03 12:56:40 +1200

    took word and split it by character into new array

--- hang-man/Program.cs
+++ hang-man/Program.cs
@@ -7,10 +7,11 @@ namespace hangman
         public static void Main(string[] args)
         {
             string wordToGuess = "frog";
-            var wordAsArray = wordToGuess.Split('');
+            char[] wordAsArray = wordToGuess.ToCharArray();
+            Console.WriteLine(string.Join(",", wordAsArray)); // prints each item of array to view in console (by joining with ,)
             string[] workingArray = new string[0];
-            string displayWord = "";
-            int playerTurnCount = 0;
+            //string displayWord = "";
+            //int playerTurnCount = 0;
             Console.WriteLine("Let's play Hangman! Guess the word.");
         }
     }
